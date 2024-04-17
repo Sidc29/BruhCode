5ef2e85795b91e0aa7d51a0faeb558d0dca86231
Shiddharth Choudhari <sidc29@protonmail.com>
2024-04-17 13:20:31 +0530

    webhook fixes

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -69,9 +69,9 @@ export async function deleteUser(params: DeleteUserParams) {
     // and wuestions, asnwers, comments, etc
 
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
