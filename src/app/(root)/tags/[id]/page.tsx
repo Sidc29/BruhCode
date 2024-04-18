514b3bcbe67883f6a5fa2e850f66932228f13a34
Shiddharth Choudhari <sidc29@protonmail.com>
2024-04-18 16:52:18 +0530

    implement tag page

---
+++ src/app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,61 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResults from "@/components/shared/no-results/NoResults";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { IQuestion } from "@/database/question.model";
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+import { URLProps } from "@/types";
+import React from "react";
+const TagDetails = async ({ params, searchParams }: URLProps) => {
+  const result: any = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route="/"
+          iconPosition="right"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result?.questions.length > 0 ? (
+          result?.questions.map((question: any) => {
+            return (
+              <QuestionCard
+                key={question._id}
+                _id={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author}
+                upvotes={question.upvotes}
+                views={question.views}
+                answers={question.answers}
+                createdAt={question.createdAt}
+              />
+            );
+          })
+        ) : (
+          <NoResults
+            title="There's no saved question to show"
+            description=" Be the first to break the silence! 🚀 Ask a Question and kickstart the
+      discussion. Our query could be the next big thing others learn from.Get
+      involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask Question"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default TagDetails;
