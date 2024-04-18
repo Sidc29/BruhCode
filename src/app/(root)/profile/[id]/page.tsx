7d0bd217faa656beafd987c481e0e4111fe084ef
Shiddharth Choudhari <sidc29@protonmail.com>
2024-04-18 19:08:40 +0530

    build fix

--- src/app/(root)/profile/[id]/page.tsx
+++ src/app/(root)/profile/[id]/page.tsx
@@ -75,8 +75,8 @@ const ProfileDetails = async ({ params, searchParams }: URLProps) => {
         </div>
       </div>
       <Stats
-        totalQuestions={userInfo?.totalQuestions}
-        totalAnswers={userInfo?.totalAnswers}
+        totalQuestions={userInfo?.totalQuestions ?? 0}
+        totalAnswers={userInfo?.totalAnswers ?? 0}
       />
       <div className="mt-10 flex gap-10">
         <Tabs defaultValue="top-posts" className="flex-1">
